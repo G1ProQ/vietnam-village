87750a78955d860a6d48d9a8da959570cca0dad5
unknown <quangzzzzz2710@gmail.com>
2022-10-25 21:15:47 +0700

    1st

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const LangNghe = require("./models/langNghe");
+// const ErrorHandle = require("./ErrorHandle");
+
+//rút gọn đường dẫn đến thư mục
+app.set("views", path.join(__dirname, "views"));
+//lựa chọn cách kết xuất file (quên rồi, để search lại)
+app.set("view engine", "ejs");
+// lấy thông tin từ form (req.body)
+app.use(express.urlencoded({ extended: true }));
+//phải serve các file tĩnh như css như thế này thì mới được
+// app.use('/public', express.static('public'));
+app.use(express.static(__dirname + "/public"));
+
+//kết nối database
+mongoose
+  .connect("mongodb://localhost:27017/langNghe")
+  .then(() => {
+    console.log("mongo connected");
+  })
+  .catch((err) => {
+    console.log("OH nooo, error");
+    console.log(err);
+  });
+
+// hàm xử lý async
+function catchAsync(fn) {
+  return function (req, res, next) {
+    fn(req, res, next).catch((e) => next(e));
+  };
+}
+
+//home page
+app.get(
+  "/",
+  catchAsync(async (req, res, next) => {
+    const langNghes = await LangNghe.find({});
+    res.render("home", {langNghes} );
+  })
+);
+//new page
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.get(
+  "/show/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const langNghe = await LangNghe.findById(id);
+    if (!langNghe) {
+      throw new Error("khong tim thay lang nghe");
+    }
+    res.render("show", { langNghe });
+  })
+);
+
+// tạo làng mới
+app.post(
+  "/",
+  catchAsync(async (req, res, next) => {
+    console.log(req.body);
+    const newVillage = new LangNghe(req.body);
+    await newVillage.save();
+    res.redirect(`/${newVillage._id}`);
+  })
+);
+//show 1 làng
+// app.get(
+//   "/:id",
+//   catchAsync(async (req, res, next) => {
+//     const { id } = req.params;
+//     const village = await LangNghe.findById(id);
+//     if (!village) {
+//     //   throw new ErrorHandle("Khong tim thay lang nghe, 404");
+//     }
+//     res.render("show", { village });
+//   })
+// );
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
---
+++ test.js
@@ -0,0 +1,13 @@
+var myobj = JSON.parse(JSON.stringify( {
+    _id: "62fb750be17e03b3bfee0790",
+    name: 'Phuc Loc',
+    address: 'Ninh Bình',
+    history: 'Since ancient times, Phuc Loc workers have created carpentry products with sophisticated carvings to serve the construction of churches, temples, communal houses, pagodas,... Meeting the great demands of people during the 90s, Phuc Loc villagers started producing civil goods such as doors, tables, chairs, cabinets, traps, bars, stair handrails, interior decoration goods... and gradually introduced fine handicrafts (inlaid goods) into production.\r\n',
+    product: 'In order to develop the craft village and bring economic efficiency, the village has transformed the highly specialized carpentry tools. The carpentry stages such as planning, polishing, turning, spraying paint... are replaced by machines to improve product quality and free up labor. At the same time, the newly replaced machines also reduce environmental pollution, especially noise pollution, and limit the risks caused by occupational accidents. However, with wooden products with small and delicate textures, no machine can replace the talented and skillful hands of Phuc Loc carpentry artisans.\r\n',
+    introduction: 'Phuc Loc Craft Village in Ninh Phong Ward, Ninh Binh City is a famous traditional craft village specializing in the production of wooden handicrafts and household products. Craft villages were born hundreds of years ago, developed in the direction of hereditary and ranked in the list of 50 most representative traditional craft villages in Vietnam. \r\n',
+    link1: 'D:\\web basic\\langNghe\\assets\\img\\LangPhucLoc1.jpg',
+    link2: 'D:\\web basic\\langNghe\\assets\\img\\LangPhucLoc2.jpg',
+    link3: 'D:\\web basic\\langNghe\\assets\\img\\LangPhucLoc3.jpg',
+    __v: 0
+  }));
+console.log((myobj.name));
